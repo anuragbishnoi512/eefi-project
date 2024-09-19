33fd7688cb2b4abdee25831bbab51f61187042fb
anurag-512 <anuragsharan51@gmail.com>
2024-09-19 10:55:21 +0530

    section edit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <div className=''>
+         <h1>anurag</h1> 
+    </div>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "20px",
+        screens: {
+          'sm': "640px",
+          'md': "768px",
+          'lg': "1024px",
+          'xl': "1180px",
+        }
+      },
+      fontSize: {
+        'custom-lg': "28px",
+        'custom-xl': "46px",
+        'custom-2xl': "85px",
+      },
+      colors: {
+        'light-blue': "#1643C3",
+        'off-blue': "#1643C31A",
+        'medium-blue': "#4273FF",
+        'dark-blue': "#002F93",
+      },
+      boxShadow: {
+        'custom-xl': "4.33px 4.33px 16.55px 0px #FFFFFF42"
+      },
+      lineHeight: {
+        'custom-xl': "55px",
+      },
+      backgroundImage: {
+        'roadmap-layer': "url('./assets/images/webp/roadmap-layer.webp')",
+        'game-layer': "url('./assets/images/webp/game-layer.webp')",
+        'stretch-layer': "url('./assets/images/webp/amplify-bg.webp')",
+      },
+      fontFamily: {
+        'plusJakarta': "plus,sans-serif",
+        'raleway': "raleway,sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
